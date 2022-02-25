597df917dee456d0a01a7f1767fdfcb914a5139d
Guilherme <bergmanguilherme2k@gmail.com>
2022-02-24 22:21:55 -0300

    basic css

--- components/CompetitionItem/CompetitionItem.jsx
+++ components/CompetitionItem/CompetitionItem.jsx
@@ -1,5 +1,11 @@
+import styles from './CompetitionItem.module.css'
+
 const CompetitionItem = ({ name }) => (
-    <li>{name}</li>
+    <li className={styles.item}>
+        <img src="https://upload.travelawaits.com/ta/uploads/2021/04/aerial-view-of-rio-de-janeiroee4454.jpg" />
+        <h3>{name}</h3>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </li >
 )
 
 export default CompetitionItem
--- components/CompetitionItens/CompetitionItens.jsx
+++ components/CompetitionItens/CompetitionItens.jsx
@@ -1,7 +1,8 @@
 import { CompetitionItem } from '..';
+import styles from './CompetitionItens.module.css'
 
 const CompetitionItens = () => (
-    <ul>
+    <ul className={styles.list}>
         <CompetitionItem name="C1" />
         <CompetitionItem name="C2" />
         <CompetitionItem name="C3" />

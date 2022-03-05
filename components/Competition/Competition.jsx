bbdb9705ee3b938c567665579e7adbd9e1706362
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-05 11:17:17 -0300

    add arrow to competition

--- components/Competition/Competition.jsx
+++ components/Competition/Competition.jsx
@@ -2,10 +2,13 @@ import styles from './Competition.module.css'
 import React, { useRef, useEffect, useState } from "react";
 import { Ranking } from '..'
 import { getHumanReadableDate } from '../../utils/dates';
+import Image from 'next/image'
 
 const Competition = ({ id, start_at, end_at, participants, title, key, ...rest }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [height, setHeight] = useState(0);
+    const [arrowRotation, setArrowRotation] = useState(0)
+    const arrowSize = 40
     const handleFilterOpening = () => {
         setIsOpen((prev) => !prev);
     }
@@ -13,17 +16,40 @@ const Competition = ({ id, start_at, end_at, participants, title, key, ...rest }
     const ref = useRef(null);
 
     useEffect(() => {
-        if (isOpen) setHeight(ref.current?.getBoundingClientRect().height);
-        else setHeight(0);
+        if (isOpen) {
+            setHeight(ref.current?.getBoundingClientRect().height)
+            setArrowRotation(90)
+        } else {
+            setHeight(0)
+            setArrowRotation(0)
+        }
     }, [isOpen]);
 
     return (
         <div className={styles.wholeItem} >
             <div className={styles.show} onClick={handleFilterOpening}>
-                <h3>{title}</h3>
-                <p>Participantes: {participants}</p>
-                <p>Início: {getHumanReadableDate(start_at)}</p>
-                <p>Fim: {getHumanReadableDate(end_at)}</p>
+                <div style={
+                    {
+                        marginLeft: '10px'
+                    }
+                }>
+                    <h3>{title}</h3>
+                    <p>Participantes: {participants}</p>
+                    <p>Início: {getHumanReadableDate(start_at)}</p>
+                    <p>Fim: {getHumanReadableDate(end_at)}</p>
+                </div>
+                <div style={
+                    {
+                        transform: `rotate(${arrowRotation}deg)`,
+                        transformOrigin: `center`,
+                        width: `${arrowSize}px`,
+                        height: `${arrowSize}px`,
+                        marginRight: '20px',
+                        marginLeft: '20px'
+                    }
+                }>
+                    <Image height={arrowSize} width={arrowSize} alt='Arrow' src='/arrow.svg' />
+                </div>
             </div>
             <div className={styles.hidden} style={{ height }}>
                 <div className={styles.ranking} ref={ref}>
--- components/Competitions/Competitions.jsx
+++ components/Competitions/Competitions.jsx
@@ -4,16 +4,18 @@ import styles from './Competitions.module.css'
 const competitions = [
     {
         "id": "asasdasd",
-        "title": "Competição RJ",
+        "title": "Batalha de Universidades SP",
         "start_at": "2022-01-01T09:00:00",
-        "end_at": "2022-01-08T09:00:00",
+        "end_at": "2022-10-08T09:00:00",
+        "available": true,
         "participants": "FGV, Santo Agostinho, São Paulo"
     },
     {
         "id": "asasdasdasas",
-        "title": "Competição RJ",
+        "title": "Essa aqui é meme",
         "start_at": "2022-01-01T09:00:00",
         "end_at": "2022-01-08T09:00:00",
+        "available": false,
         "participants": "FGV, Santo Agostinho, São Paulo"
     },
     {
@@ -21,14 +23,15 @@ const competitions = [
         "title": "Competição RJ",
         "start_at": "2022-01-01T09:00:00",
         "end_at": "2022-01-08T09:00:00",
+        "available": false,
         "participants": "FGV, Santo Agostinho, São Paulo"
     }
 ]
 
 const Competitions = () => (
     <div className={styles.list}>
-        {competitions.map(({ id, title, start_at, end_at, participants, ...rest }) => (
-            <Competition key={id} title={title} id={id} start_at={start_at} end_at={end_at} participants={participants} />
+        {competitions.map(({ id, title, start_at, end_at, participants, available, ...rest }) => (
+            <Competition key={id} title={title} id={id} start_at={start_at} end_at={end_at} participants={participants} available={available} />
         ))}
     </div>
 )

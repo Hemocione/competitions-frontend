8d8b06fe662c6475accb6294bd99465027aea7e0
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-15 20:03:02 -0300

    add header content type

--- utils/api.js
+++ utils/api.js
@@ -4,7 +4,10 @@ const { NEXT_PUBLIC_BACKEND_URL } = process.env;
 const apiClient = axios.create(
   {
     baseURL: NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080",
-    responseType: 'json'
+    responseType: 'json',
+    headers: {
+      'Content-Type': 'application/json'
+    }
   }
 )
 

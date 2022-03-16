99c32783da1f0f1b6045539d8fb3bf798a7bd29f
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-16 02:54:31 -0300

    please.

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home({competitions}) {
   )
 }
 
+// please work
 export const getServerSideProps = async () => {
   const competitionsRes = await getCompetitions()
   const competitions = await competitionsRes.data

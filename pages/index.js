112b440b8e47c8cec13310f8c126dfbbee83a4da
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-16 00:43:42 -0300

    try catch on serversideprops

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,18 @@ export default function Home({competitions}) {
 }
 
 export const getServerSideProps = async () => {
-  const url = new URL('/competitions', process.env.NEXT_PUBLIC_BACKEND_URL);
-  const competitionsRes = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  const competitions = await competitionsRes.json()
-
-  return { props: { competitions: competitions } }
+  try {
+    const url = new URL('/competitions', process.env.NEXT_PUBLIC_BACKEND_URL);
+    const competitionsRes = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const competitions = await competitionsRes.json()
+    return { props: { competitions: competitions } }
+  } catch(err) {
+    console.log(err)
+    return { props: { competitions: [] } }
+  }
 }

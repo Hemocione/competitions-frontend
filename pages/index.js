78d9db8fadbc7e9479b26844063f0633a03cf813
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-16 00:26:08 -0300

    change serversideprops

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,7 @@ export default function Home({competitions}) {
 }
 
 export async function getServerSideProps() {
-  const url = new URL('/competitions', process.env.NEXT_PUBLIC_BACKEND_URL)
-  const competitionsRes = await fetch(url)
+  const competitionsRes = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/competitions`)
   const competitions = await competitionsRes.json()
 
   return { props: { competitions: competitions } }

d2c7e26100a385bff340317d1748f10e95d5819b
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-16 00:08:15 -0300

    move competition status

--- components/CompetitionStatus/CompetitionStatus.jsx
+++ components/CompetitionStatus/CompetitionStatus.jsx
@@ -1,20 +1,19 @@
-// status 2 = ativo, 1 = upcoming, 0 = finalizado
-const statusMapping = {
-  0: {
-    text: 'FINALIZADO',
-    background: '#E00E16'
-  },
-  1: {
-    text: 'EM BREVE',
-    background: '#FFB655'
-  },
-  2: {
-    text: 'ACONTECENDO',
-    background: 'rgba(51, 217, 178, 1)'
-  }
-}
-
 const CompetitionStatus = ({ status, ...rest }) => {
+  // status 2 = ativo, 1 = upcoming, 0 = finalizado
+  const statusMapping = {
+    0: {
+      text: 'FINALIZADO',
+      background: '#E00E16'
+    },
+    1: {
+      text: 'EM BREVE',
+      background: '#FFB655'
+    },
+    2: {
+      text: 'ACONTECENDO',
+      background: 'rgba(51, 217, 178, 1)'
+    }
+  }
   const statusData = statusMapping[status]
 
   return (

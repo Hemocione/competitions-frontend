dd4e2f729fd305c313e6173288997e4b5e4c7d63
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-21 22:11:49 -0300

    trigger deploy

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home({competitions}) {
   )
 }
 
+// Get the competitions from the API before loading page
 export const getServerSideProps = async () => {
   const competitionsRes = await getCompetitions()
   const competitions = await competitionsRes.data

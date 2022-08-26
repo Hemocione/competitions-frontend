63fb3fffaf102fc6355a2ceeec3a24ee1ca6c635
matheusleo <matheusleotrab@gmail.com>
2022-08-25 22:27:41 -0300

    add mechanism to change between the institutions and team view of the ranking

--- components/Ranking/Ranking.jsx
+++ components/Ranking/Ranking.jsx
@@ -1,15 +1,21 @@
 import styles from './Ranking.module.css'
 import { DonationDialog, Ranker, SimpleButton } from '..'
 import { useState } from 'react'
+import { MenuItem, FormControl, Select } from '@mui/material';
 
-const Ranking = ({ competition_id, ableToDonate, ranking, ...rest }) => {
+const Ranking = ({ competition_id, ableToDonate, ranking, rankingType, onRankingTypeChange, ...rest }) => {
     const [openDialog, setOpenDialog] = useState(false)
+
     const donationClickHandler = () => (
         setOpenDialog(true)
     )
     const dialogCloseHandler = () => (
         setOpenDialog(false)
     )
+    const rankingTypeChangeHandler = (event) => {
+        onRankingTypeChange(event.target.value)
+    }
+
     return (
         <div>
             {ableToDonate ? <div className={styles.buttonBox}>
@@ -20,7 +26,30 @@ const Ranking = ({ competition_id, ableToDonate, ranking, ...rest }) => {
                 <thead>
                     <tr>
                         <th>Posição</th>
-                        <th>Time</th>
+                        <th>
+                            <FormControl>
+                               <Select
+                                labelId="ranking-type"
+                                id="ranking-type"
+                                value={rankingType}
+                                onChange={rankingTypeChangeHandler}
+                                inputProps={{ 'aria-label': 'Tipo de ranking' }}
+                                style={
+                                    {
+                                        fontFamily: "arial",
+                                        fontWeight: "bold",
+                                        padding: "9px",
+                                        backgroundColor: "#D1151A",
+                                        color: "#FFF",
+                                        borderColor: "#FFF"
+                                    }
+                                }
+                               >
+                                <MenuItem value="institutions">Instituição</MenuItem>
+                                <MenuItem value="team">Time</MenuItem>
+                               </Select>
+                            </FormControl>
+                        </th>
                         <th>Doações</th>
                     </tr>
                 </thead>
